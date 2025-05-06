43dc54bc2d7c98e2d8f8d37cf99c78f56bb253bf
Jonathan Saenger <jonathan.saenger@outlook.fr>
2025-05-07 00:03:32 +0200

    MAIN > HEADER : Implémentation du Header

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,32 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './app.css';
+import './styles/base.css';
+import './styles/layout.css';
+import './styles/components/footer.css';
+import './styles/components/header.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// Navbar pour mobile
+document.addEventListener('DOMContentLoaded', function() {
+  const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+  const navbar = document.querySelector('.navbar');
+
+  if (mobileMenuToggle) {
+      mobileMenuToggle.addEventListener('click', function() {
+          mobileMenuToggle.classList.toggle('active');
+          navbar.classList.toggle('active');
+          document.body.classList.toggle('menu-open');
+      });
+  }
+
+  document.addEventListener('click', function(event) {
+      if (navbar.classList.contains('active') &&
+          !event.target.closest('.navbar') &&
+          !event.target.closest('.mobile-menu-toggle')) {
+          mobileMenuToggle.classList.remove('active');
+          navbar.classList.remove('active');
+          document.body.classList.remove('menu-open');
+      }
+  });
+});
