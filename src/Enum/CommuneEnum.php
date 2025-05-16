016da6747d0530a2e9ed3b0e7fd2aeac54e2b7a3
Jonathan Saenger <jonathan.saenger@outlook.fr>
2025-05-17 00:32:03 +0200

    RENCONTRES > ENUM : Ajout de l'enum Commune avant la création de l'entité

---
+++ src/Enum/CommuneEnum.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+enum CategoryEnum: string
+{
+  case FERRETTE = 'Ferrette';
+  case READERSDORF = 'Readersdorf';
+  case BENDORF = 'Bendorf';
+  case LIEGSDORF = 'Liegsdorf';
+  case BIEDERTHAL = 'Biederthal';
+  case BLOTZHEIM = 'Blotzheim';
+  case BRUEBACH = 'Bruebach';
+  case CHALAMPE = 'Chalampé';
+  case CHAVANNES = 'Chavannes';
+  case CHAVANNESSURLETANG = 'Chavannes-sur-l\'Etang';
+  case CHÊNEBOURG = 'Chêne-Bourg';
+  case CHÊNEBOURGERIE = 'Chêne-Bourgeries';
+  case CHÊNEPÂQUIER = 'Chêne-Pâquier';
+  case CHÊNETHUY = 'Chêne-Thuy';
+}
