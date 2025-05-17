9f71ab240f695a0f4e17dc18e8e8315cc40f9f6a
Jonathan Saenger <jonathan.saenger@outlook.fr>
2025-05-17 12:46:51 +0200

    RENCONTRES > ENUM : Correction des Enums avant création de l'entité

--- src/Enum/CommuneEnum.php
+++ src/Enum/CommuneEnum.php
@@ -4,20 +4,43 @@ declare(strict_types=1);
 
 namespace App\Enum;
 
-enum CategoryEnum: string
+enum CommuneEnum: string
 {
-  case FERRETTE = 'Ferrette';
-  case READERSDORF = 'Readersdorf';
-  case BENDORF = 'Bendorf';
-  case LIEGSDORF = 'Liegsdorf';
-  case BIEDERTHAL = 'Biederthal';
-  case BLOTZHEIM = 'Blotzheim';
-  case BRUEBACH = 'Bruebach';
-  case CHALAMPE = 'Chalampé';
-  case CHAVANNES = 'Chavannes';
-  case CHAVANNESSURLETANG = 'Chavannes-sur-l\'Etang';
-  case CHÊNEBOURG = 'Chêne-Bourg';
-  case CHÊNEBOURGERIE = 'Chêne-Bourgeries';
-  case CHÊNEPÂQUIER = 'Chêne-Pâquier';
-  case CHÊNETHUY = 'Chêne-Thuy';
+  case FERRETTE = 'ferrette';
+  case READERSDORF = 'readersdorf';
+  case BENDORF = 'bendorf';
+  case LIEGSDORF = 'liegsdorf';
+  case BIEDERTHAL = 'biederthal';
+  case BLOTZHEIM = 'blotzheim';
+  case BRUEBACH = 'bruebach';
+  case CHALAMPE = 'chalampe';
+  case CHAVANNES = 'chavannes';
+  case CHAVANNESSURLETANG = 'chavannes_sur_letang';
+  case CHENEBOURG = 'chene_bourg';
+  case CHENEBOURGERIE = 'chene_bourgeries';
+  case CHENEPÂQUIER = 'chene_paquier';
+  case CHENETHUY = 'chene_thuy';
+
+  /**
+   * Retourne le nom d'affichage de la commune
+   */
+  public function getLabel(): string
+  {
+      return match($this) {
+          self::FERRETTE => 'Ferrette',
+          self::READERSDORF => 'Readersdorf',
+          self::BENDORF => 'Bendorf',
+          self::LIEGSDORF => 'Liegsdorf',
+          self::BIEDERTHAL => 'Biederthal',
+          self::BLOTZHEIM => 'Blotzheim',
+          self::BRUEBACH => 'Bruebach',
+          self::CHALAMPE => 'Chalampé',
+          self::CHAVANNES => 'Chavannes',
+          self::CHAVANNESSURLETANG => 'Chavannes-sur-l\'Etang',
+          self::CHENEBOURG => 'Chêne-Bourg',
+          self::CHENEBOURGERIE => 'Chêne-Bourgeries',
+          self::CHENEPÂQUIER => 'Chêne-Pâquier',
+          self::CHENETHUY => 'Chêne-Thuy',
+      };
+  }
 }
--- src/Enum/RencontreEnum.php
+++ src/Enum/RencontreEnum.php
@@ -10,35 +10,12 @@ enum RencontreEnum: string
     case PUBLIQUE = 'publiques';
     case DEPLACEMENT = 'deplacements';
 
-    /**
-     * Retourne le libellé correspondant au type
-     */
     public function getLabel(): string
     {
-        return match($this) {
+        return match ($this) {
             self::CITOYENNE => 'Rencontre citoyenne',
             self::PUBLIQUE => 'Réunion publique',
             self::DEPLACEMENT => 'Déplacement',
         };
-    }
-
-    /**
-     * Retourne tous les types sous forme de tableau associatif pour les formulaires
-     */
-    public static function getChoices(): array
-    {
-        $choices = [];
-        foreach(self::cases() as $case) {
-            $choices[$case->getLabel()] = $case->value;
-        }
-        return $choices;
-    }
-
-    /**
-     * Retourne tous les types en tant qu'objets enum
-     */
-    public static function getAllTypes(): array
-    {
-        return self::cases();
     }
 }
